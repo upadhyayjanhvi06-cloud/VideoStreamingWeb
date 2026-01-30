f7abf622accb11d76cca1146dccb1debfe36255c
Ansh Singh <anshsingh150804>
2026-01-30 13:32:21 +0530

    Basic Setup

--- server.js
+++ server.js
@@ -2,21 +2,52 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
-// allow static files (CSS)
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-// home page
+// fake database
+let videos = [
+  {
+    id: 1,
+    title: "My First Video",
+    filename: "sample.mp4",
+    likes: 0,
+    uploader: "admin"
+  },
+  {
+    id: 2,
+    title: "Learning Node.js",
+    filename: "sample2.mp4",
+    likes: 0,
+    uploader: "admin"
+  }
+];
+
+// HOME PAGE
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// watch page
-app.get("/watch", (req, res) => {
-  res.send("<h2>Watch Page (coming soon)</h2>");
+// WATCH PAGE
+app.get("/watch/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "watch.html"));
 });
 
-// start server
+// UPLOAD PAGE
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "upload.html"));
+});
+
+// LOGIN PAGE
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+// DASHBOARD
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "dashboard.html"));
+});
+
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
