1661c65cc543c3a839ebda842a4c60a05462a40d
Ansh Singh <anshsingh150804>
2026-01-30 14:09:10 +0530

    uploading feature added

--- server.js
+++ server.js
@@ -5,37 +5,43 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: "public/videos",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+
 // fake database
 let videos = [
-  {
-    id: 1,
-    title: "My First Video",
-    filename: "sample.mp4",
-    likes: 0,
-    uploader: "admin"
-  },
-  {
-    id: 2,
-    title: "Learning Node.js",
-    filename: "sample2.mp4",
-    likes: 0,
-    uploader: "admin"
-  }
+  { id: 1, title: "My First Video", filename: "sample.mp4", likes: 0 },
+  { id: 2, title: "Learning Node.js", filename: "sample2.mp4", likes: 0 }
 ];
+
 
 // HOME PAGE
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", { videos });
 });
+
 
 // WATCH PAGE
 app.get("/watch/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "watch.html"));
+  const video = videos.find(v => v.id == req.params.id);
+  res.render("watch", { video });
 });
+
 
 // UPLOAD PAGE
 app.get("/upload", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "upload.html"));
+  res.sendFile(path.join(__dirname, "views", "upload.ejs"));
 });
 
 // LOGIN PAGE
@@ -48,6 +54,28 @@ app.get("/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "dashboard.html"));
 });
 
+app.post("/like/:id", (req, res) => {
+  const video = videos.find(v => v.id == req.params.id);
+  video.likes++;
+  res.redirect("/watch/" + video.id);
+});
+
+
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
+});
+
+app.get("/upload", (req, res) => {
+  res.render("upload");
+});
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  videos.push({
+    id: videos.length + 1,
+    title: req.body.title,
+    filename: req.file.filename,
+    likes: 0
+  });
+
+  res.redirect("/");
 });
